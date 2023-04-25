ef6cab288b8cc731f3b73e922902a927faad9989
Danillo <danillodesouza03@gmail.com>
2023-04-24 22:48:18 -0300

    Create elif.py

---
+++ 06 if aninhado/elif.py
@@ -0,0 +1,13 @@
+# Exemplo de estruta do ELIF
+
+nome = input("Digite seu nome: ")
+if nome == "Danillo":
+    print ("Acesso autorizado", nome)
+elif nome == "Thaís":
+    print ("Acesso autorizado", nome)
+elif nome == "Allan":
+    print ("Acesso autorizado", nome)
+elif nome == "Isabelly":
+    print ("Acesso autorizado", nome)
+else:
+    print ("Acesso Negado.")
