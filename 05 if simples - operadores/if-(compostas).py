4743c83422ae9b414570dcfd1421b451b7d94f78
Danillo <danillodesouza03@gmail.com>
2023-04-24 22:43:35 -0300

    Aula 05

---
+++ 05 if simples - operadores/if-(compostas).py
@@ -0,0 +1,7 @@
+# Estrutura simples IF 
+temperatura = (int(input("Digite a temperatura: ")))
+if temperatura>25:
+    print("Ambiente quente, ajustando ar-condicionado.")
+else:
+    print("Ambiente ok, não precisa ser ajustado.")
+print("ligando o ar-condicionado...")    
---
+++ 05 if simples - operadores/if-(simples).py
@@ -0,0 +1,12 @@
+# Estrutura simples IF 
+temperatura = (int(input("Digite a temperatura: ")))
+if temperatura>25:
+    print("Ambiente quente, ajustando ar-condicionado.")
+
+if temperatura==25:
+    print("Temperatura ambiente.")
+
+if temperatura<25:
+    print("Ambiente frio, ajustando ar-condicionado.")
+    
+print("ligando o ar-condicionado...")    
