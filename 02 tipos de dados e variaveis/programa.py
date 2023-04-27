5042053e71e3a84e0c8e1be832538c3a48c0424b
Danillo <danillodesouza03@gmail.com>
2023-04-27 09:03:25 -0300

    create 02

---
+++ 02 tipos de dados e variaveis/programa.py
@@ -0,0 +1,37 @@
+"""
+Começar com uma letra ou sublinhado.
+Não começar com um dígito.
+Ter no máximo 256 caracteres.
+Letras, dígitos, sublinhados e cifrões podem ser inseridos.
+Não conter espaços e símbolos matemáticos (+, -, /, *, parenteses).
+
+bool: só assume dois valores, True (verdadeiro) e False (falso). O
+Python associa o número 1 ao valor True e 0 ao valor False.
+
+int: esses objetos possuem precisão arbitrária, ou seja, podem exibir
+números com quantidade arbitrária de dígitos, sendo poossível calcular
+números cujo resultado é um valor bem considerável.
+
+float: representa números reais racionais, que permitem a representação na codificação ponto flutuante, com 64 dígitos binários de
+precisão. Nessa classe, usa-se o caractere “.” para separar a parte inteira
+da decimal, e não “,”.
+
+complex: representa números complexos. A letra “j”, maiúscula ou
+minúscula, é utilizada para representar o número imaginário.
+
+None (NoneType): objeto que satisfaz como lugar reservado vazio;
+é um objeto que apenas ocupa espaço. O None pode ser utilizado para
+representar algo que, no momento, é desconhecido, mas que futuramente
+será conhecido.
+
+str: dado utilizado para representar texto; sequências de caracteres
+(strings) são declaradas a partir do uso de aspas simples ou duplas, de
+modo equivalente.
+
+tuple: são apresentadas com parênteses (opcionais), e os elementos
+são separados por “,”.
+
+type: objetos utilizados para a manipulação de tipos.
+"""
+primeiraVariavel = True 
+type(primeiraVariavel)
