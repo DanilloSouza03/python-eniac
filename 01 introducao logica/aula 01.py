d652c01444abf71af6e3a75b1b08cdb90773dc74
Danillo <danillodesouza03@gmail.com>
2023-04-27 08:34:41 -0300

    Create aula 01.py

---
+++ 01 introducao logica/aula 01.py
@@ -0,0 +1,10 @@
+#Primeiro Código em Python
+
+nome = input("Nome do aluno? ")
+P1 = float(input("Entre com a nota da Prova 1 "))
+P2 = float(input("Entre com a nota da Prova 2 "))
+
+media=(P1+P2)/2
+
+print("ALUNO: ",nome)
+print("Média = ",media)
